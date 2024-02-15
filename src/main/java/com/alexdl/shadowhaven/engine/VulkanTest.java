067501b724517880b00961993fec1a3ecc2a0115
Aleksander Długosz <olekdlugi@gmail.com>
2024-02-15 23:44:33 +0100

    Added glfw & split awt / glfw test beds

--- src/main/java/com/alexdl/shadowhaven/engine/VulkanTest.java
+++ src/main/java/com/alexdl/shadowhaven/engine/VulkanTest.java
@@ -1,155 +1,27 @@
 package com.alexdl.shadowhaven.engine;
 
-
-import com.formdev.flatlaf.FlatDarkLaf;
-import org.lwjgl.PointerBuffer;
-import org.lwjgl.awt.AWT;
 import org.lwjgl.system.MemoryStack;
-import org.lwjgl.vulkan.KHRSurface;
-import org.lwjgl.vulkan.VkApplicationInfo;
-import org.lwjgl.vulkan.VkInstance;
-import org.lwjgl.vulkan.VkInstanceCreateInfo;
-import org.lwjgl.vulkan.awt.AWTVK;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.nio.IntBuffer;
 
-import static org.lwjgl.vulkan.EXTDebugReport.VK_ERROR_VALIDATION_FAILED_EXT;
-import static org.lwjgl.vulkan.KHRDisplaySwapchain.VK_ERROR_INCOMPATIBLE_DISPLAY_KHR;
-import static org.lwjgl.vulkan.KHRSurface.*;
-import static org.lwjgl.vulkan.KHRSwapchain.VK_ERROR_OUT_OF_DATE_KHR;
-import static org.lwjgl.vulkan.KHRSwapchain.VK_SUBOPTIMAL_KHR;
-import static org.lwjgl.vulkan.VK10.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.vulkan.VK10.vkEnumerateInstanceExtensionProperties;
 
 public class VulkanTest {
-    private static VkInstance createInstance() {
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            VkApplicationInfo appInfo = VkApplicationInfo
-                    .calloc(stack)
-                    .sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
-                    .pApplicationName(stack.UTF8("AWT Vulkan Demo"))
-                    .pEngineName(stack.UTF8(""))
-                    .apiVersion(VK_MAKE_VERSION(1, 0, 2));
+    public static void main(String[] args) {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        long window = glfwCreateWindow(800, 600, "Vulkan Test", 0, 0);
 
-            PointerBuffer ppEnabledExtensionNames = stack.pointers(stack.UTF8(VK_KHR_SURFACE_EXTENSION_NAME), stack.UTF8(AWTVK.getSurfaceExtensionName()));
 
-            VkInstanceCreateInfo pCreateInfo = VkInstanceCreateInfo
-                    .calloc(stack)
-                    .sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
-                    .pApplicationInfo(appInfo)
-                    .ppEnabledExtensionNames(ppEnabledExtensionNames);
-
-            PointerBuffer pInstance = stack.mallocPointer(1);
-            int err = vkCreateInstance(pCreateInfo, null, pInstance);
-            if (err != VK_SUCCESS) {
-                throw new RuntimeException("Failed to create VkInstance: " + translateVulkanResult(err));
-            }
-
-            long instance = pInstance.get(0);
-            return new VkInstance(instance, pCreateInfo);
-
+        try(MemoryStack memoryStack = MemoryStack.stackPush()) {
+            IntBuffer extensionCount = memoryStack.mallocInt(1);
+            vkEnumerateInstanceExtensionProperties((String) null, extensionCount, null);
+            System.out.printf("Extension count: %d", extensionCount.get(0));
         }
-    }
-    public static void main(String[] args) throws AWTException {
-        if (!AWT.isPlatformSupported()) {
-            throw new RuntimeException("Platform not supported.");
-        }
-        System.setProperty("apple.awt.application.appearance", "system");
-        FlatDarkLaf.setup();
 
-        VkInstance instance = createInstance();
-
-        JFrame frame = new JFrame("Editor");
-        frame.setBackground(Color.black);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        Canvas canvas = new Canvas();
-        canvas.setPreferredSize(new Dimension(600, 600));
-
-        JPanel options = new JPanel();
-        options.setLayout(new BoxLayout(options, BoxLayout.Y_AXIS));
-        options.add(new JButton("Some button"));
-        options.add(new JTextField("Input something!"));
-
-        JPanel panel = new JPanel();
-        panel.add(options);
-        panel.add(canvas);
-
-        frame.add(panel);
-        frame.pack();
-        long surface = AWTVK.create(canvas, instance);
-
-        frame.setVisible(true);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-
-                // Destroy the surface to prevent leaks and errors
-                KHRSurface.vkDestroySurfaceKHR(instance, surface, null);
-            }
-        });
-    }
-
-    public static String translateVulkanResult(int result) {
-        switch (result) {
-            // Success codes
-            case VK_SUCCESS:
-                return "Command successfully completed.";
-            case VK_NOT_READY:
-                return "A fence or query has not yet completed.";
-            case VK_TIMEOUT:
-                return "A wait operation has not completed in the specified time.";
-            case VK_EVENT_SET:
-                return "An event is signaled.";
-            case VK_EVENT_RESET:
-                return "An event is unsignaled.";
-            case VK_INCOMPLETE:
-                return "A return array was too small for the result.";
-            case VK_SUBOPTIMAL_KHR:
-                return "A swap chain no longer matches the surface properties exactly, but can still be used to present to the surface successfully.";
-
-            // Error codes
-            case VK_ERROR_OUT_OF_HOST_MEMORY:
-                return "A host memory allocation has failed.";
-            case VK_ERROR_OUT_OF_DEVICE_MEMORY:
-                return "A device memory allocation has failed.";
-            case VK_ERROR_INITIALIZATION_FAILED:
-                return "Initialization of an object could not be completed for implementation-specific reasons.";
-            case VK_ERROR_DEVICE_LOST:
-                return "The logical or physical device has been lost.";
-            case VK_ERROR_MEMORY_MAP_FAILED:
-                return "Mapping of a memory object has failed.";
-            case VK_ERROR_LAYER_NOT_PRESENT:
-                return "A requested layer is not present or could not be loaded.";
-            case VK_ERROR_EXTENSION_NOT_PRESENT:
-                return "A requested extension is not supported.";
-            case VK_ERROR_FEATURE_NOT_PRESENT:
-                return "A requested feature is not supported.";
-            case VK_ERROR_INCOMPATIBLE_DRIVER:
-                return "The requested version of Vulkan is not supported by the driver or is otherwise incompatible for implementation-specific reasons.";
-            case VK_ERROR_TOO_MANY_OBJECTS:
-                return "Too many objects of the type have already been created.";
-            case VK_ERROR_FORMAT_NOT_SUPPORTED:
-                return "A requested format is not supported on this device.";
-            case VK_ERROR_SURFACE_LOST_KHR:
-                return "A surface is no longer available.";
-            case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-                return "The requested window is already connected to a VkSurfaceKHR, or to some other non-Vulkan API.";
-            case VK_ERROR_OUT_OF_DATE_KHR:
-                return "A surface has changed in such a way that it is no longer compatible with the swapchain, and further presentation requests using the "
-                       + "swapchain will fail. Applications must query the new surface properties and recreate their swapchain if they wish to continue"
-                       + "presenting to the surface.";
-            case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-                return "The display used by a swapchain does not use the same presentable image layout, or is incompatible in a way that prevents sharing an"
-                       + " image.";
-            case VK_ERROR_VALIDATION_FAILED_EXT:
-                return "A validation layer found an error.";
-            default:
-                return String.format("%s [%d]", "Unknown", result);
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
         }
     }
 }
